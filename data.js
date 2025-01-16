89dae58d39c5b2a060849499ac2e808ab0ffcc1c
Leander-Arun Ziehm <Leander-arun.ziehm@stud.th-deg.de>
2025-01-16 14:02:01 +0100

    .

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = {
+	"intro":"",
+	"questions": [
+{ text: "Ich stehe Mitten auf dem Campus. Ich bin kein Gebäude, aber trotzdem stabil. Wenn du hoch hinaus willst, steigst du auf mich.", answer: "Campustreppe" ,info:"x"},
+{ text: "Dort, wo ihr jetzt steht, könnt ihr mich nicht sehen. Ich bin ein Haus, in dem es meistens leise ist. Wenn ihr lernen müsst, braucht ihr das, was in mir wohnt. Man nennt mich auch BIBB.", answer: "Bibliothek" ,info:"x"},
+{ text: "Suche die richtige Fakultät! An dieser Fakultät sammelst du viel Praxiserfahrung. Ohne meine Studiengänge bzw. ohne die Menschen, die sowas lernen, gäbe es keine Bauwerke. Welche Gebäude nummer hat es?", answer: "D" ,info:"x"},
+{ text: "In diesem Gebäude hängt ein großes Pendel von der Decke. Man geht hinein und sieht es gleich.", answer: "L" ,info:"x"},
+{ text: "Auf dem Neucampus befinden sich Sitzgelegenheiten/Liegen in unterschiedlichen Farben. Wie viele sind es insgesamt?", answer: "28" ,info:"x"},
+{ text: "In welchem Gebäude befindet sich der Terminal, an dem die Studierenden den Studentenausweis aufladen können.", answer: "B" ,info:"x"},
+{ text: "Gehe zu dem Gebäude, das ein Fernsehstudio hat, weil die Studierenden dieser Fachrichtung alles über Medien lernen.", answer: "J" ,info:"x"},
+{ text: "Dieses Gebäude ist keine Fakultät. Es gibt nur Vorlesungssäle, in denen unterrichtet wird. In dem Gebäude befindet sich eine Bildergalerie. Schaue dir die Bilder genau an. Worin sitzt Dr. Wolfgang Heubisch?", answer: "Fast Forest" ,info:"x"},
+{ text: "Bei uns könnt ihr kostengünstig etwas Essen. Mittagsgerichte und Snacks gibt es in diesem Gebäude.", answer: "Mensa" ,info:"x"}
+]
+}
---
+++ g.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DW1EF1FQP8"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-DW1EF1FQP8');
+</script>
